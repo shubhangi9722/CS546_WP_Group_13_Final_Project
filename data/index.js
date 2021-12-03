31bc4b79b949da393a0b533295bc8a4bc7cc2a9f
tapish97 <55229455+tapish97@users.noreply.github.com>
2021-12-02 21:32:47 -0500

    2nd commit

--- data/index.js
+++ data/index.js
@@ -1,7 +1,8 @@
 const customerData = require('./customerData');
 const sitterData = require('./sitterData');
-
+const bookingData = require('./bookingData');
 module.exports = {
     customer: customerData,
-    sitter: sitterData
+    sitter: sitterData,
+    booking:bookingData
 };
